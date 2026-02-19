19fb1b58356f5d80f917d3a5925b5e77f1791730
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2026-02-19 05:34:08 +0000

    ..

---
+++ Textutilis/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+   <>
+  <Navbar title="Textutilis" AboutText="AboutText Utilis" />
+  <div className="container my-3">
+ <TextForm heading="Enter your text here"/> 
+  </div>
+ 
+
+   </> 
+  );
+}
+
+
+export default App;
+
